767d598ff3d85a2a4ed5691d50a044f706132d14
Fernando Antunes <f.antunes84@gmail.com>
2022-11-07 17:02:09 -0300

    Removendo o arquivo menu.py e as funções de menu

--- repositories/os_repository.py
+++ repositories/os_repository.py
@@ -49,19 +49,3 @@ def decode():
     os.environ['TOKEN'] = keys['token']
     for i in range(1, 6):
         os.environ[f'KEY{i}'] = keys[f'key{i}']
-
-
-def create_dotenv_file():
-    decode()
-    with open('.env', 'w', encoding='utf-8') as file:
-        file.write(f'URL = {os.getenv("URL")}\n')
-        file.write(f'TOKEN = {os.getenv("TOKEN")}\n')
-        for i in range(1, 6):
-            file.write(f'KEY{i} = {os.getenv(f"KEY{i}")}\n')
-        file.close()
-
-
-def clean_dotenv_file():
-    os.remove('.env')
-    with open('.env', 'w', encoding='utf-8') as file:
-        file.close()
